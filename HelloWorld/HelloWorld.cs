41d5f4f4326b0271a69f472d987ad5f5f7c285b1
Pascal Groß <pascal.gross@student.fh-kiel.de>
2022-01-16 12:29:32 +0100

    added comments; finished the HelloWorld example

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,12 +1,12 @@
+// These first lines of code mark the beginning of my journey through the C# program language.
+// I took the LinkedIn Learning course 'Learning C#' by Joe Marini: https://www.linkedin.com/learning/learning-c-sharp-8581491/an-introduction-to-learning-c-sharp
+// I wrote this code in Microsoft Visual Studio Enterprise 2019 IDE
+// Date (format YYYY-MM-DD): 2022-01-16
+
 using System;
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+
+Console.WriteLine("Hello World!");
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine($"Why, hello there {name}.");
